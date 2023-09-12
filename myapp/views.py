d217138c5c33f37415e6a2ce4ec7fcd0cca2b29f
NurudeenNajeem <80988295+NurudeenNajeem@users.noreply.github.com>
2023-09-12 14:40:37 +0100

    First commit

---
+++ myapp/views.py
@@ -0,0 +1,31 @@
+from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from datetime import date,datetime,timezone
+# from .serializer import Serializer
+# Create your views here.
+datetime.utcnow()
+@api_view()
+def api(request):
+     curent_day = datetime.today().strftime('%A')
+
+     context = [
+           {
+          "slack_name":"Nurudeen",
+          "current_day":curent_day,
+          # "utc_time":datetime.now(),
+          # "utc_tim":datetime.utcnow(),
+          "utc_time": datetime.now(timezone.utc),
+          "track": "backend",
+          "github_file_url": "https://github.com/NurudeenNajeem/django",
+          "github_file_repo":"github.com/NurudeenNajeem",
+          "status_code":status.HTTP_200_OK
+
+     }
+
+     ]
+    
+     return Response(context)
+
+
